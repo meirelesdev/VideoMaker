d6034f8f802f310cbe52180c83f9cc7b604c08f8
Daniel Meireles <httpdaniel@yahoo.com.br>
2020-08-18 06:23:25 -0300

    Robot image initialized

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 const robots = {
     input: require('./robots/input'),
     text: require('./robots/text'),
-    state: require('./robots/state')
+    state: require('./robots/state'),
+    image: require('./robots/image')
 }
 
 const start = async ()=> {
-    robots.input()
-    await robots.text()
+    // robots.input()
+    // await robots.text()
+    await robots.image()
     
     const content = robots.state.load()
     console.dir(content, { depth: null })
